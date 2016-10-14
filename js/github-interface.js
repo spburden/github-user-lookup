a2fc60d0fdd88e1f0512b5c4825b1b6afa54bb74
Stephen Burden <spburden@hotmail.com>
2016-10-13 21:36:04 -0700

    change api address to display the usernames repo info

--- js/github-interface.js
+++ js/github-interface.js
@@ -5,8 +5,11 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var username = $("#username").val();
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    $('#username').val("");
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
       console.log(response);
+      console.log(response[17].name);
+      console.log(response[17].description);
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
