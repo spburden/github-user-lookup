f38776eb002e6d2697a26f10dff84ce6096d9c28
Stephen Burden <spburden@hotmail.com>
2016-10-14 16:05:48 -0700

    refactor code to use callback functions

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,4 +1,3 @@
-var apiKey = require('./../.env').apiKey;
 var User = require('./../js/github.js').userModule;
 
 $(document).ready(function(){
@@ -14,9 +13,47 @@ $(document).ready(function(){
     $("#avatar").attr("src", '');
     var username = $("#username").val();
     var newUser = new User();
-    newUser.getRepos(username);
-    newUser.getInfo(username);
+    newUser.getRepos(username, displayUserRepos);
+    newUser.getInfo(username, displayUserInfo);
     $(".jumbotron").show();
     $("#username").val("");
   });
 });
+
+var displayUserRepos = function(username, response) {
+  if(response.length){
+    if (response.length > 1) {
+      $("#inputUsername").html("<h2>" + username + "</h2>");
+      $(".username").append(username + "'s " + response.length + " Public Repositories:");
+    } else {
+      $(".username").append(username + "'s Public Repository:");
+    }
+    for (var i = 0; i < response.length; i++) {
+      $(".result").append("<h4>Repo Name: " + response[i].name + "</h4>");
+      if (response[i].description) {
+        $(".result").append("<h5>Description: " + response[i].description + "</h5>");
+      } else {
+        $(".result").append("<h6>No Description</h6>");
+      }
+      $(".result").append("<h5>Date created: " + moment(response[i].created_at).format("dddd MMM Do YYYY") + "</h5><br>");
+    }
+  } else {
+    $(".username").text(username + " does not have any Public Repositories yet!");
+  }
+};
+
+var displayUserInfo = function(username, response) {
+  if(response){
+    if(response.followers){
+      $("#followers").append("Followers: " + response.followers);
+      $("#following").append("Following: " + response.following + "<br>");
+    }
+    if(response.avatar_url){
+      $("#avatar").attr("src", response.avatar_url);
+      $("#avatar").show();
+    }
+    if(response.name){
+      $("#nameOfUser").append(response.name);
+    }
+  }
+};
--- js/github.js
+++ js/github.js
@@ -1,31 +1,11 @@
 var apiKey = require('./../.env').apiKey;
 
 function User() {
-
 }
 
-User.prototype.getRepos = function(username) {
+User.prototype.getRepos = function(username, displayUserRepos) {
   $.get('https://api.github.com/users/' + username + '/repos?sort=created&per_page=200&access_token=' + apiKey).then(function(response){
-    console.log(response);
-    if(response.length){
-      if (response.length > 1) {
-        $("#inputUsername").html("<h2>" + username + "</h2>");
-        $(".username").append(username + "'s " + response.length + " Public Repositories:");
-      } else {
-        $(".username").append(username + "'s Public Repository:");
-      }
-      for (var i = 0; i < response.length; i++) {
-        $(".result").append("<h4>Repo Name: " + response[i].name + "</h4>");
-        if (response[i].description) {
-          $(".result").append("<h5>Description: " + response[i].description + "</h5>");
-        } else {
-          $(".result").append("<h6>No Description</h6>");
-        }
-        $(".result").append("<h5>Date created: " + moment(response[i].created_at).format("dddd MMM Do YYYY") + "</h5><br>");
-      }
-    } else {
-      $(".username").text(username + " does not have any Public Repositories yet!");
-    }
+    displayUserRepos(username, response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
     $(".username").text(username + " was not found!");
@@ -33,21 +13,9 @@ User.prototype.getRepos = function(username) {
   });
 };
 
-User.prototype.getInfo = function (username) {
+User.prototype.getInfo = function (username, displayUserInfo) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    if(response){
-      if(response.followers){
-        $("#followers").append("Followers: " + response.followers);
-        $("#following").append("Following: " + response.following + "<br>");
-      }
-      if(response.avatar_url){
-        $("#avatar").attr("src", response.avatar_url);
-        $("#avatar").show();
-      }
-      if(response.name){
-        $("#nameOfUser").append(response.name);
-      }
-    }
+    displayUserInfo(username, response);
   });
 };
 
